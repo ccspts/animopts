5a6bd8980dc0b80e6ee55d1cd417d86ebeea6841
Albert <lorenzdiz@gmail.com>
2017-02-05 12:44:43 +0800

    Decoupled database connection properties to an external xml file named "dbconfig.xml" located at "src/dbconfig.xml"

--- src/org/animopts/db/DBPool.java
+++ src/org/animopts/db/DBPool.java
@@ -3,24 +3,64 @@ package org.animopts.db;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class DBPool {
 
-	public final static String url = "jdbc:mysql://localhost:3306/animopts";
-	public final static String username = "root";
-	public final static String password = "password";
-	public final static String driver = "com.mysql.jdbc.Driver";
-
+	private static String DB_FILE = "dbconfig.xml";
 	private static DBPool dbPool = null;
 	private static BasicDataSource basicDataSource;
 
 	private DBPool() {
+		System.out.println("Establishing database connection pool");
+		Document doc = null;
+		try {
+			System.out.println("Loading database config file: dbconfig.xml");
+			ClassLoader classLoader = this.getClass().getClassLoader();
+			InputStream input = classLoader.getResourceAsStream(DB_FILE);
+			
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			doc = dBuilder.parse(input);
+		} catch (IOException e) {
+			System.out.println("File not found(" + DB_FILE + ")");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String url = "";
+		String username = "";
+		String password = "";
+		String driver = "";
+		
+		try {
+			Element database = doc.getDocumentElement();
+			url = database.getElementsByTagName("url").item(0).getTextContent();
+			username = database.getElementsByTagName("username").item(0).getTextContent();
+			password = database.getElementsByTagName("password").item(0).getTextContent();
+			driver = database.getElementsByTagName("driver").item(0).getTextContent();
+		} catch (DOMException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("User(" + username + ") Connecting to (" + url + ") using (" + driver + ")");
 		basicDataSource = new BasicDataSource();
 		basicDataSource.setDriverClassName(driver);
 		basicDataSource.setUrl(url);
 		basicDataSource.setUsername(username);
 		basicDataSource.setPassword(password);
+		
+		System.out.println("SUCCESS::Database connection pool established");
 	}
 
 	public static DBPool getInstance() {
@@ -37,7 +77,6 @@ public class DBPool {
 		try {
 			return basicDataSource.getConnection();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
